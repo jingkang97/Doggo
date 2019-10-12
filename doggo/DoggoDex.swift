00a15ad8a8867a5d7755fdb986a763940dca8b6e
Jing Kang <ngjingkang97@gmail.com>
2019-10-13 01:29:48 +0800

    added pictures

---
+++ doggo/DoggoDex.swift
@@ -0,0 +1,18 @@
+//
+//  DoggoDex.swift
+//  doggo
+//
+//  Created by Ng Jing Kang on 13/10/19.
+//  Copyright © 2019 Ng Jing Kang. All rights reserved.
+//
+
+import UIKit
+
+class DoggoDex :UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
