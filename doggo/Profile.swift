2e2e3a7bd604834ef0b5cf4ce3e58fe8208a9be3
Jing Kang <ngjingkang97@gmail.com>
2019-10-13 03:32:40 +0800

    create icon

---
+++ doggo/Profile.swift
@@ -0,0 +1,17 @@
+//
+//  Profile.swift
+//  doggo
+//
+//  Created by Ng Jing Kang on 13/10/19.
+//  Copyright © 2019 Ng Jing Kang. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class Profile : UIViewController {
+        
+    
+    
+}
+
