9f2d8d482fdbe9040beeb1948ab2695e7c01daaf
Jing Kang <ngjingkang97@gmail.com>
2019-10-12 15:55:20 +0800

    first commit

---
+++ doggo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  doggo
+//
+//  Created by Ng Jing Kang on 12/10/19.
+//  Copyright © 2019 Ng Jing Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
